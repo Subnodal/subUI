7523fc4b42f8d900541f85b37fbc93124546b820
James-Livesey <jamesliveg@gmail.com>
2021-07-12 18:42:33 +0100

    Add basic style design

---
+++ subui.js
@@ -0,0 +1,12 @@
+/*
+    subUI
+
+    Copyright (C) Subnodal Technologies. All Rights Reserved.
+
+    https://subnodal.com
+    Licenced by the Subnodal Open-Source Licence, which can be found at LICENCE.md.
+*/
+
+namespace("com.subnodal.subui", function(exports) {
+    
+});
