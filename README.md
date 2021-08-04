301e5f1c864c22ebe4d7c363744b3c4a39ee4b84
James-Livesey <jamesliveg@gmail.com>
2021-08-04 17:27:22 +0100

    Add distribution system

--- README.md
+++ README.md
@@ -2,3 +2,29 @@
 A dynamic UI library for creating desktop and mobile applications.
 
 Licenced by the [Subnodal Open-Source Licence](LICENCE.md).
+
+## Dev environment/distribution
+Ensure that you have [subPack](https://github.com/Subnodal/subPack) and
+[subDoc](https://github.com/Subnodal/subDoc) installed first. The `dist.py`
+script will then invoke subPack and subDoc when building, and will also
+additionally minify the CSS files for distribution.
+
+To test or distribute subUI, run:
+
+```bash
+$ python3 dist.py
+```
+
+You'll need to install the `csscompressor` dependency if you haven't already:
+
+```bash
+$ python3 -m pip install csscompressor
+```
+
+### Distributing on Windows
+Use `py -3` instead of `python3`, since the latter is generally for UNIX
+systems.
+
+### For Subnodalers only
+When distributing to the [Subnodal CDN](https://cdn.subnodal.com/), the `dist/`
+directory should be copied to the CDN under the directory name `lib/subui/`.
---
+++ dist.py
@@ -0,0 +1,49 @@
+import re
+import os
+import os.path
+import shutil
+import csscompressor
+
+MAIN_CSS_FILE = "style.css"
+DIST_DIR = "dist"
+
+COPY_PATHS = [
+    ["fonts", "fonts", True],
+    ["media", "media", True],
+    ["build/subui.min.js", "subui.min.js", False]
+]
+
+def replaceImport(replacer):
+    minifyAndResolve(replacer.group(1))
+
+    return replacer.group().replace(".css", ".min.css")
+
+def minifyAndResolve(inPath):
+    outPath = os.path.join(DIST_DIR, inPath.replace(".css", ".min.css"))
+
+    os.makedirs(os.path.dirname(outPath), exist_ok = True)
+
+    unminified = open(inPath, "r")
+    unminifiedCode = unminified.read()
+    minified = open(outPath, "w")
+
+    unminifiedCode = re.sub(r"@import url\(\"(.*)\"\)", replaceImport, unminifiedCode)
+
+    minified.write(csscompressor.compress(unminifiedCode))
+
+    unminified.close()
+    minified.close()
+
+shutil.rmtree(DIST_DIR)
+os.system("subdoc")
+os.system("subpack")
+
+for copyPath in COPY_PATHS:
+    pastePath = os.path.join(DIST_DIR, *copyPath[1].split("/"))
+
+    if copyPath[2]:
+        shutil.copytree(os.path.join(*copyPath[0].split("/")), pastePath)
+    else:
+        shutil.copy2(os.path.join(*copyPath[0].split("/")), pastePath)
+
+minifyAndResolve(MAIN_CSS_FILE)
