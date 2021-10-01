b1e6a0b1b2926b439c05371d264539f19e6a7e6f
James-Livesey <jamesliveg@gmail.com>
2021-10-01 19:45:56 +0100

    Add docs for platform code

---
+++ docs/en/reference/com.subnodal.subui.platforms.md
@@ -0,0 +1,74 @@
+# com.subnodal.subui.platforms
+## 🔒️ `browserEngineTypes`
+`const <{*}>` · Enum for types of browser engine.
+
+## 🔒️ `browserEngineTypes.CHROMIUM`
+`const <*>` · The Chromium browser engine.
+
+## 🔒️ `browserEngineTypes.GECKO`
+`const <*>` · The Gecko browser engine.
+
+## 🔒️ `browserEngineTypes.OTHER`
+`const <*>` · An unknown browser engine.
+
+## 🔒️ `browserEngineTypes.WEBKIT`
+`const <*>` · The WebKit browser engine.
+
+## 🔒️ `browserTypes`
+`const <{*}>` · Enum for types of browser.
+
+## 🔒️ `browserTypes.CHROMIUM`
+`const <*>` · The Google Chrome or Chromium web browsers.
+
+## 🔒️ `browserTypes.EDGE`
+`const <*>` · The Microsoft Edge web browser. 
+
+This is the new Microsoft Edge, based on Chromium, and not the old
+EdgeHTML-based Edge.
+
+## 🔒️ `browserTypes.FIREFOX`
+`const <*>` · The Mozilla Firefox web browser.
+
+## 🔒️ `browserTypes.OPERA`
+`const <*>` · The Opera web browser.
+
+## 🔒️ `browserTypes.ORBIT`
+`const <*>` · The Subnodal Orbit web browser.
+
+## 🔒️ `browserTypes.OTHER`
+`const <*>` · An unknown browser.
+
+## 🔒️ `browserTypes.SAFARI`
+`const <*>` · The Apple Safari web browser.
+
+## ▶️ `getPlatform`
+`function` · Get the current device's platform information.
+
+**Returns:** `{os: osTypes, browserEngine: browserEngineTypes, browser: browserTypes}` · An object representing the current platform.
+
+## 🔒️ `osTypes`
+`const <{*}>` · Enum for types of operating systems.
+
+## 🔒️ `osTypes.ANDROID`
+`const <*>` · A generic flavour of the Android operating system.
+
+## 🔒️ `osTypes.CHROMEOS`
+`const <*>` · The Google Chrome OS operating system.
+
+## 🔒️ `osTypes.IOS`
+`const <*>` · The Apple iOS or Apple iPadOS operating systems.
+
+## 🔒️ `osTypes.LINUX`
+`const <*>` · A generic GNU/Linux distribution.
+
+## 🔒️ `osTypes.MACOS`
+`const <*>` · The Apple macOS operating system.
+
+## 🔒️ `osTypes.OTHER`
+`const <*>` · An unknown operating system.
+
+## 🔒️ `osTypes.SUBOS`
+`const <*>` · The Subnodal subOS operating system.
+
+## 🔒️ `osTypes.WINDOWS`
+`const <*>` · The Microsoft Windows operating system.
--- platforms.js
+++ platforms.js
@@ -9,8 +9,51 @@
 
 // @namespace com.subnodal.subui.platforms
 namespace("com.subnodal.subui.platforms", function(exports) {
-    // TODO: Add docs
-
+    /*
+        @name osTypes
+        @type const <{*}>
+        Enum for types of operating systems.
+    */
+    /*
+        @name osTypes.OTHER
+        @type const <*>
+        An unknown operating system.
+    */
+    /*
+        @name osTypes.SUBOS
+        @type const <*>
+        The Subnodal subOS operating system.
+    */
+    /*
+        @name osTypes.LINUX
+        @type const <*>
+        A generic GNU/Linux distribution.
+    */
+    /*
+        @name osTypes.WINDOWS
+        @type const <*>
+        The Microsoft Windows operating system.
+    */
+    /*
+        @name osTypes.MACOS
+        @type const <*>
+        The Apple macOS operating system.
+    */
+    /*
+        @name osTypes.IOS
+        @type const <*>
+        The Apple iOS or Apple iPadOS operating systems.
+    */
+    /*
+        @name osTypes.CHROMEOS
+        @type const <*>
+        The Google Chrome OS operating system.
+    */
+    /*
+        @name osTypes.ANDROID
+        @type const <*>
+        A generic flavour of the Android operating system.
+    */
     exports.osTypes = {
         OTHER: -1,
         SUBOS: 0,
@@ -22,6 +65,31 @@ namespace("com.subnodal.subui.platforms", function(exports) {
         ANDROID: 6
     };
 
+    /*
+        @name browserEngineTypes
+        @type const <{*}>
+        Enum for types of browser engine.
+    */
+    /*
+        @name browserEngineTypes.OTHER
+        @type const <*>
+        An unknown browser engine.
+    */
+    /*
+        @name browserEngineTypes.CHROMIUM
+        @type const <*>
+        The Chromium browser engine.
+    */
+    /*
+        @name browserEngineTypes.GECKO
+        @type const <*>
+        The Gecko browser engine.
+    */
+    /*
+        @name browserEngineTypes.WEBKIT
+        @type const <*>
+        The WebKit browser engine.
+    */
     exports.browserEngineTypes = {
         OTHER: -1,
         CHROMIUM: 0,
@@ -29,6 +97,49 @@ namespace("com.subnodal.subui.platforms", function(exports) {
         WEBKIT: 2
     };
 
+    /*
+        @name browserTypes
+        @type const <{*}>
+        Enum for types of browser.
+    */
+    /*
+        @name browserTypes.OTHER
+        @type const <*>
+        An unknown browser.
+    */
+    /*
+        @name browserTypes.ORBIT
+        @type const <*>
+        The Subnodal Orbit web browser.
+    */
+    /*
+        @name browserTypes.CHROMIUM
+        @type const <*>
+        The Google Chrome or Chromium web browsers.
+    */
+    /*
+        @name browserTypes.FIREFOX
+        @type const <*>
+        The Mozilla Firefox web browser.
+    */
+    /*
+        @name browserTypes.SAFARI
+        @type const <*>
+        The Apple Safari web browser.
+    */
+    /*
+        @name browserTypes.EDGE
+        @type const <*>
+        The Microsoft Edge web browser.
+        ~~~~
+            This is the new Microsoft Edge, based on Chromium, and not the old
+            EdgeHTML-based Edge.
+    */
+    /*
+        @name browserTypes.OPERA
+        @type const <*>
+        The Opera web browser.
+    */
     exports.browserTypes = {
         OTHER: -1,
         ORBIT: 0,
@@ -39,6 +150,11 @@ namespace("com.subnodal.subui.platforms", function(exports) {
         OPERA: 5
     };
 
+    /*
+        @name getPlatform
+        Get the current device's platform information.
+        @returns <{os: osTypes, browserEngine: browserEngineTypes, browser: browserTypes}> An object representing the current platform.
+    */
     exports.getPlatform = function() {
         var platform = {
             os: exports.osTypes.OTHER,
